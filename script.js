e881c77b81f8afcf708c2384cb24f4206b9bd118
Justin Albert <jalbert@carthage.edu>
2020-09-24 14:38:44 -0500

    finished adding functions

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
+  document.getElementById("password").innerHTML = generatePassword();
 
 }
 
@@ -16,12 +17,14 @@ var useNum;
 var useSpec;
 var lengthOfPass;
 
+// Prompt for length of password.
 function determineLength(){
   lengthOfPass=prompt("How many characters would you like you password to be?");
   lengthOfPass=Number(lengthOfPass);
   checkLength();
 }
 
+// Check length for valid input.
 function checkLength(){
   if(!(Number.isInteger(lengthOfPass) && (lengthOfPass > 0))){
     alert("The length of your password must be a natural number.");
@@ -30,9 +33,6 @@ function checkLength(){
 
 }
 
-// determineLength();
-// console.log("Length is "+lengthOfPass);
-// console.log(Number.isInteger(lengthOfPass));
 
 function determineCharacters(){
 useCap=confirm("Do you want capital letters?");
@@ -51,11 +51,7 @@ while( !(useCap || useLow || useNum || useSpec)){
 }
 }
 
-// determineCharacters();
-// console.log(useCap);
-// console.log(useLow);
-// console.log(useNum);
-// console.log(useSpec);
+
 
 function randomCapitalLetter(){
   var a=Math.floor(Math.random()*26)+65;
@@ -63,8 +59,7 @@ function randomCapitalLetter(){
   return a;
 }
 
-// var b=randomCapitalLetter();
-// console.log(b);
+
 
 function randomLowerLetter(){
   var a=Math.floor(Math.random()*26)+97;
@@ -72,26 +67,49 @@ function randomLowerLetter(){
   return a;
 }
 
-// var c=randomLowerLetter();
-// console.log(c);
+
 
 function randomSpecialCharacter(){
-  var a=Math.floor(Math.random()*15)+97;
+  var a=Math.floor(Math.random()*4)+35;
   a="&#"+a;
   return a;
 }
 
-// var d=randomSpecialCharacter();
-// console.log(d);
 
 function randomNumber(){
-  var a=Math.floor(Math.random()*15)+97;
+  var a=Math.floor(Math.random()*10)+48;
   a="&#"+a;
   return a;
 }
 
-// var e=randomNumber();
-// console.log(e);
+
+
+function generatePassword(){
+  determineLength();
+  determineCharacters();
+  var pass="";
+
+for(i=0; i<lengthOfPass; i++){
+
+  var numb=Math.floor(Math.random()*4);
+  if(numb===0 && useCap){
+    pass=pass+" "+randomCapitalLetter();
+  }
+  if(numb===1 && useLow){
+    pass=pass+" "+randomLowerLetter();
+  }
+  if(numb===2 && useNum){
+    pass=pass+" "+randomNumber();
+  }
+  if(numb===3 && useSpec){
+    pass=pass+" "+randomSpecialCharacter();
+  }
+}
+
+return pass;
+
+}
+
 
 
 // Add event listener to generate button
